2dd7c15e0b5baa39ed018d5693e857dba0bdd111
ryaand <bahriansyahherlambang3@gmail.com>
2026-06-20 15:37:59 +0700

    chore(deps): install motion library

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
 import Image from "next/image";
+import * as motion from "motion/react-client"
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <motion.div
+      initial={{ opacity: 0, y: -40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, ease: "easeInOut" }}
+      className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"
+    >
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
@@ -60,6 +66,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-    </div>
+    </motion.div>
   );
 }
