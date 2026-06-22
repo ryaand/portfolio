570e4889a0478a94ffadb91651f8c27538c7605e
ryaand <bahriansyahherlambang3@gmail.com>
2026-06-22 14:50:59 +0700

    refractor: fix footer links

--- src/Components/Utilities/Footer/index.js
+++ src/Components/Utilities/Footer/index.js
@@ -12,7 +12,7 @@ const Footer = () => {
                 </div>
                 <div className="flex gap-3">
                     <Link className="opacity-50 hover:opacity-100 transition-all duration-300" href="https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRpctRWvSnMpJjtFlWCxldFvCHlsbMgBRLZrzHJWLWQtkxKdPNDjZHZbFLcCkPTqDfKSPhg"><Image alt="email" src={Email} width={20}></Image></Link>
-                    <Link className="opacity-50 hover:opacity-100 transition-all duration-300" href="https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRpctRWvSnMpJjtFlWCxldFvCHlsbMgBRLZrzHJWLWQtkxKdPNDjZHZbFLcCkPTqDfKSPhg"><Image alt="email" src={Github} width={20}></Image></Link>
+                    <Link className="opacity-50 hover:opacity-100 transition-all duration-300" href="https://github.com/ryaand"><Image alt="github" src={Github} width={20}></Image></Link>
                 </div>
             </div>
         </section>
