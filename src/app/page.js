0eb5331cce0dcd8cb317c0fe963447fe05422c1d
ryaand <bahriansyahherlambang3@gmail.com>
2026-06-22 13:51:33 +0700

    fix: set always on top when refresh the web

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,15 @@ import HeroSection from "@/Components/HeroSection";
 import ProjectSection from "@/Components/ProjectSection";
 import SkillsSection from "@/Components/SkillsSection";
 import Footer from "@/Components/Utilities/Footer";
+import { useState } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual'
+    }
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <section className="overflow-x-hidden">
       <HeroSection></HeroSection>
