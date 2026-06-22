ea124fe89fa9b40d90cfc1d9a24bc04d55874666
ryaand <bahriansyahherlambang3@gmail.com>
2026-06-22 09:17:44 +0700

    refractor: set interactive LightDefaultButton2 when clicking on phone

--- src/Components/Utilities/LightDefaultButton2/index.js
+++ src/Components/Utilities/LightDefaultButton2/index.js
@@ -5,7 +5,7 @@ import { motion } from "motion/react"
 const LightDefaultButton2 = ({ children }) => {
     return (
         <div>
-            <button className="border-[0.5px] border-zinc-400/30 rounded-full px-5 py-1.5 hover:bg-black/90 z-50 hover:text-white transition-all duration-300">
+            <button className="border-[0.5px] border-zinc-400/30 rounded-full px-5 py-1.5 hover:bg-black/90 focus:bg-black/90 focus:text-white z-50 hover:text-white transition-all duration-300">
             {children}
             </button>
         </div>
